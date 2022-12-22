bdad5aab212228bf5e86c143bca52e08b3f8b546
Joshua <joshf2797@gmail.com>
2022-12-21 18:37:04 -0700

    got the vin function working

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var submit = document.getElementById('submitvin');
+var vin = document.getElementById('vin');
+
+
+submit.addEventListener('click', function(event) {
+  var vinInput = document.getElementById("vininput").value;
+  var carApiKey = `https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVin/${vinInput}*BA?format=json`
+  
+  event.preventDefault()
+  fetch(carApiKey, {
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+
+    localStorage.setItem('VIN', vinInput)
+    
+  })
+
+  vin.value = localStorage.getItem('VIN')
+
+
+
