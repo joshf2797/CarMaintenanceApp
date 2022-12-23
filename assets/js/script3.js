d2c2d11429764769714fe24576fca39d1f788255
Marcus Paccapaniccia <marcuspacc@gmail.com>
2022-12-22 21:06:19 -0700

    Updated script.js and script.js3 to reflect updated code and code comments

--- assets/js/script3.js
+++ assets/js/script3.js
@@ -1,8 +1,18 @@
-var apiKey = "04a66eeb-1c3a-4bd4-8703-0567b5f60751"
-// var carChargingUrl = `https://api.openchargemap.io/v3/poi/?key=${apiKey}`
-var carChargingUrl = `https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=1000&key=${apiKey}`
+// api key for our account
+var apiKey = "04a66eeb-1c3a-4bd4-8703-0567b5f60751";
+// link to the api we are fetching data from with the api key within it
+var carChargingUrl = `https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=1000&key=${apiKey}`;
+// variable called cycleZip that is set to the submitZip class
+var cycleZip = document.querySelector(".submitZip");
+// variable called displayZip that is set to the enteredZip ID
+var displayZip = document.querySelector("#enteredZip");
+// variable called displayAddress that is set to the enteredAddress ID
+var displayAddress = document.querySelector("#enteredAddress");
+// variable called displayTitle that is set to the enteredTitle ID
+var displayTitle = document.querySelector("#enteredTitle");
 
-function compareZip() {
+// funtion that pulls the data from the api and returns the response in json.  Then runs the zipCompare function
+function runApi() {
     fetch(carChargingUrl, {
         mode: "cors", 
         headers: {
@@ -13,44 +23,45 @@ function compareZip() {
         return response.json();
       })
       .then(function (data) {
-        // Make sure to look at the response in the console and read how 404 response is structured.
         console.log(data);
-        console.log(data[4].AddressInfo.Postcode);
-        zipCompareCycle (data)
-      })
-    };
+        zipCompare (data)
+      });
+};
 
-function zipCompareCycle(data) {
+//function that compares the user entered zip with the data from the API
+function zipCompare(data) {
   var userZip = document.getElementById("zipInput").value;
+  //for loop that runs the each object in the data and stores them in objects with three key-value pairs
   for (var i = 0; i < data.length; i++) {
       var zipCode = data[i].AddressInfo.Postcode
       var dataObj = {
       address: data[i].AddressInfo.AddressLine1,
       name: data[i].AddressInfo.Title,
       zip: data[i].AddressInfo.Postcode
-      }
+      };
+      //If statement to compare the entered Zipcode from the user with the zip codes from the API.  Runs the addText function if there is a match and exits the loop
       if (zipCode == userZip) {
-          console.log(dataObj)
           addText (dataObj);
           return
       }
+
       else {
         displayZip.innerHTML = "No Results Found"
         displayAddress.innerHTML = ""
         displayTitle.innerHTML = ""
       }
-  }
-}
-var cycleZip = document.querySelector(".submitZip");
+  };
+};
+
+//Event listener on a click to run the runApi Function
 cycleZip.addEventListener("click", function() {
-  compareZip ()
+  runApi ()
 });
-var displayZip = document.querySelector("#enteredZip");
-var displayAddress = document.querySelector("#enteredAddress");
-var displayTitle = document.querySelector("#enteredTitle");
+
+//Function to add text to the HTML to display the zip code, address, and name of the charging location if it matches the user inputted zipcode
 function addText (dataObj) {
   displayZip.innerHTML = dataObj.zip
   displayAddress.innerHTML = dataObj.address
   displayTitle.innerHTML = dataObj.name
-}
+};
 
